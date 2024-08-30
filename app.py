4d33ff3e5cf16a693a9ccd6e31967fe94e7df42f
Nishan Kamath <nishankamath@gmail.com>
2024-08-30 12:48:39 +0530

    newcommit

--- app.py
+++ app.py
@@ -176,4 +176,6 @@ def get_whether():
 
     return render_template('show_whether.html',desc=desc,temp=temp,name=name,humidity=humidity,sea_level=sea_level)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
